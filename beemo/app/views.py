67a979feb5a3b64315125d5e0258822780ab52e9
Colin Scott-Fleming <colinsf@email.arizona.edu>
2014-01-26 12:42:00 -0700

    built out index template to be more extensible

--- beemo/app/views.py
+++ beemo/app/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+# Setup menu items
+menu_items = [
+    {'title': 'home', 'url': '/'},
+]
+
+# Setup dashboard items
+dashboard_items = [
+
+]
+    
+
+
+class BeemoIndex(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(BeemoIndex, self).get_context_data(*args, **kwargs)
+
+        context['menu_items'] = menu_items if len(menu_items) > 0 else None
+        context['dashboard_items'] = dashboard_items if len(dashboard_items) > 0 else None
+
+        return context
