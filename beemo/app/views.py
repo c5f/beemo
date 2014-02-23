664c2f228ac1dbc5549a1fa810ee1818a1899fea
Colin Scott-Fleming <colinsf@email.arizona.edu>
2014-02-23 16:13:26 -0700

    added some base javascript objects for participants and calls.

--- beemo/app/views.py
+++ beemo/app/views.py
@@ -1,23 +1,20 @@
 from django.views.generic import TemplateView
 
 
-# Setup menu items
-menu_items = [
-    {'title': 'home', 'url': '/'},
-]
-
-# Setup dashboard items
-dashboard_items = [
-
-]
-    
-
-
 class BeemoIndex(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, *args, **kwargs):
+
         context = super(BeemoIndex, self).get_context_data(*args, **kwargs)
+
+        menu_items = [
+            {'title': 'home', 'url': '/'},
+        ]
+
+        recent_calls = {'title': 'Recent Calls', 'width': 'fullwidth', 'type': 'd3-bar-chart'}
+        
+        dashboard_items = [recent_calls]
 
         context['menu_items'] = menu_items if len(menu_items) > 0 else None
         context['dashboard_items'] = dashboard_items if len(dashboard_items) > 0 else None
