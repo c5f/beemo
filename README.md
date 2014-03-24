fa309735c9b06735fe92d332920a527047943cd9
Colin Scott-Fleming <colinsf@email.arizona.edu>
2014-03-24 13:43:44 -0700

    moved static root back to project dir for now.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Behavior Modeler for LIvES 
 
-**Be[e]**havior **Mo**deler is a Django project designed to use [NLTK](http://nltk.org) and [Orange](http://orange.biolab.si) to extract and examine trends in participant and coach behavior within the [LIvES Project](http://ovarianlives.org).
+**Be[e]**havior **Mo**deler is a Django project designed to apply a [_k_-means clustering](http://en.wikipedia.org/wiki/K-means_clustering) analysis to extract and examine trends in participant and coach behavior within the [LIvES Project](http://ovarianlives.org).
 
 ## Dependencies
 
--- beemo/beemo/settings.py
+++ beemo/beemo/settings.py
@@ -77,7 +77,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '~/Code/beemo/beemo/app/static/app/'
+STATIC_ROOT = os.getcwd() + '/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -88,6 +88,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.getcwd() + '/static/app/',
 )
 
 # List of finder classes that know how to find static files in
