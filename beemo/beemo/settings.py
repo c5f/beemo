d4934bbfd937958910666ade30df24642a2360b5
Colin Scott-Fleming <colinsf@email.arizona.edu>
2014-03-24 19:10:45 -0700

    added template context processors
    according to https://docs.djangoproject.com/en/1.5/ref/contrib/admin/, certain
    template context processors need to be enabled for the admin functionality to
    behave properly.

--- beemo/beemo/settings.py
+++ beemo/beemo/settings.py
@@ -109,6 +109,17 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+# A tuple of callables that are used to populate the context in RequestContext.
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages'
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
