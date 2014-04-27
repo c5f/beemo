8c625927fbeb15d578cafda8b15e33d276f6e527
Colin Scott-Fleming <colinsf@email.arizona.edu>
2014-04-26 23:11:53 -0700

    Add dashboard graph to main view.

--- beemo/app/static/app/scripts/controllers/dashboard.js
+++ beemo/app/static/app/scripts/controllers/dashboard.js
@@ -6,9 +6,30 @@ app.controller('DashboardCtrl', function ($scope, Restangular) {
         // TODO: Remove this binding
         window.scope = $scope
 
-        // Setup
+        // Bind the call data to $scope.
         Restangular.setBaseUrl('/api/');
-        $scope.dashboardCalls = []
+        $scope.dashboardCalls = [];
 
-        $scope.dashboardCalls = Restangular.all('calls').getList().$object;
+        // Bind the chart data to another $scope variable to render the chart.
+        $scope.dashboardData = [{
+            key: "Recent Calls",
+            values: [[1,1],[2,0.8]]
+        }];
+
+        Restangular.all('calls').getList().
+            then(function (calls) {
+                calls = calls.slice(0, 50);
+
+                $scope.dashboardCalls = calls;
+
+                var dashboardData = calls.map(function (call, index) {
+                    if (call.adherence_score != null) {
+                        return [index, call.adherence_score];
+                    } else {
+                        return [index, 0];
+                    }
+                });
+
+                $scope.dashboardData[0].values = dashboardData;
+            });
     });
